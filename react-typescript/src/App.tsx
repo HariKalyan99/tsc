370546add2e670dcc242abbe8dce919012afee69
Harikalyan <harikalyan99tronics@gmail.com>
2026-02-13 18:21:29 +0530

    initiated a vite/react tailwind style-guide

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-red-600">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
